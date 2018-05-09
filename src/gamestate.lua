126ff3746133112a83a467ac0af658bf23f088c2
tonetheman <tonetheman@gmail.com>
2018-05-09 07:28:22 -0500

    using grid

--- src/gamestate.lua
+++ src/gamestate.lua
@@ -2,14 +2,19 @@
 GameState = Class{__includes = BaseState}
 
 function GameState:init()
-    self.blocks = {}
+    self.grid = Grid(2,2)
     local color={ r=255,g=0,b=0}
-    self.blocks[1] = Block(color,10,10)
+    for i=1,2 do
+        for j=1,2 do
+            self.grid:set(i,j,Block(color,i*32,j*32))
+        end
+    end
 end
 
 
 function GameState:render()
-    for i=1,#self.blocks do
-        self.blocks[i]:render()
-    end
+    --for i=1,#self.blocks do
+    --    self.blocks[i]:render()
+    --end
+    self.grid:render()
 end
