634a351253d064af7d49c70f0f6050e2fb62575c
tonetheman <tonetheman@gmail.com>
2018-05-09 07:27:35 -0500

    size change

--- src/block.lua
+++ src/block.lua
@@ -11,5 +11,5 @@ function Block:render()
     love.graphics.setColor(
         self.color.r,self.color.g,self.color.b)
     love.graphics.rectangle("fill",
-        self.x,self.y,32,32)
+        self.x,self.y,16,16)
 end
